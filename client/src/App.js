e79b844761d6466efd8cc0787604b033fb3953de
Vivica A. Blakeney <vivica.blakeney@AdminisatorsMBP.attlocal.net>
2023-04-10 14:49:21 -0400

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,31 @@
-import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
-import React from 'react';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import Plans from './pages/Plans';
-import Products from './pages/Products';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import './App.css';
-
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Plans from "./pages/Plans";
+import Products from "./pages/Products";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
-  
   return (
-  <div className="App"> 
-<Router>
-  <Navbar/>
+    <div className="App">
+      <Navbar />
+      <Router>
+      
 
-  <Routes>
-       <Route path='/' element={ <Home/> }/>    
-       {/* <Route path='/services' element={<Services/>}/>  */}
-       <Route path='/plans' element={<Plans/>}/>
-       <Route path='/products' element={<Products/>}/>
-       <Route path='/contact' element={< Contact/> }/> 
-       </Routes>  
-       </Router>
-       <Footer/>
-    
-  </div>
-      );
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path='/services' element={<Services/>}/>  */}
+          <Route path="/plans" element={<Plans />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
