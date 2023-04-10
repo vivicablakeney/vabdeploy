88ced8060c242dc9bc10ab4e9d3c890d466613a4
Vivica A. Blakeney <vivica.blakeney@AdminisatorsMBP.attlocal.net>
2023-04-10 15:56:49 -0400

    ihave faith

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,34 +1,40 @@
 import { Component } from "react";
 import React from "react";
-import '../Styles.css/NavbarStyles.css';
+import { Link } from "react-router-dom";
+import "../Styles.css/NavbarStyles.css";
 
 class Navbar extends Component {
-state = { navOpen: false };
+  state = { navOpen: false };
 
-render() {
-return (
-<nav>
-<div className='logo'> <img src="images/logo1.png" alt="logo"></img></div>
-<ul id="navbar" className={this.state.navOpen ? "active" : ""}>
-<li>
-<a className="active" href="/">Home</a>
-</li>
-{/* <li>
+  render() {
+    return (
+      <nav>
+        <div className="logo">
+          {" "}
+          <img src="images/logo1.png" alt="logo"></img>
+        </div>
+        <ul id="navbar" className={this.state.navOpen ? "active" : ""}>
+          <li>
+            <Link className="active" to ="/">
+              Home
+            </Link>
+          </li>
+          {/* <li>
 <a href="/services">Services</a>
 </li> */}
-<li>
-<a href="/plans">Service</a>
-</li>
-<li>
-<a href="/products">Shop</a>
-</li>
-<li>
-<a href="/contact">Contact</a>
-</li>
-</ul>
-</nav>
-);
-}
+          <li>
+          <Link to="/plans">Plans</Link>
+        </li>
+          <li>
+            <Link to="/products">Shop</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
